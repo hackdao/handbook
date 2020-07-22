ef7c06a3af50c3f75153cda28bf5a411d998d663
苏涛 <first@taosue.xyz>
2020-07-23 01:52:13 +0800

    update

--- sidebars.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  someSidebar: {
-    // "Introduction": ["overview"],
-    // "Light House": ["onboard", "raiding"],
-    // "Field Manual": ["roles", "rooms", "glossary", "library"]
-    "DAO指南": ["overview"],
-  }
-};
